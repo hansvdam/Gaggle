517d3b97bb38a4450a9e05b895a7626e69a8616b
hansvdam <git@damsoft.nl>
2012-12-14 16:09:28 +0100

    commiting an eclipse project

---
+++ data.js
@@ -0,0 +1,19 @@
+// If you add an archive the lines in this archive adher to the following logic:
+// x1, y1, x2, y2, LabelOfTheArea,ColorOfTheArea,ColorOfTheLabel,UrlOfTheArchive
+//
+// where:
+//
+//   (x1,y1)  +-------------------------+
+//            |                         |
+//            |                         |
+//            |                         |
+//            |                         |
+//            |                         |
+//            |                         |
+//            +-------------------------+  (x2,y2)
+//
+var dataArray = [
+[6.37207,46.080852,7.822265,45.560218,"Chamonix","#FF0000","#000000","http://www.damsoft.nl/MyChamonix.mbtiles"]
+];
+// function getData(){
+// return [[6.37207,46.080852,7.822265,45.560218,"Chamonix","#FF0000","#000000","http://www.damsoft.nl/MyChamonix.mbtiles"]];}
